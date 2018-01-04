e5443794414fbe05503faee5e722774d84654d02
baolong.lai <baolong.lai@tpv-tech.com>
2018-01-04 14:32:44 +0800

    Initial Commit

---
+++ AdaptationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AdaptationDemo
+//
+//  Created by tpv tpv on 18/1/4.
+//  Copyright © 2018年 xumxum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AdaptationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AdaptationDemo
+//
+//  Created by tpv tpv on 18/1/4.
+//  Copyright © 2018年 xumxum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
